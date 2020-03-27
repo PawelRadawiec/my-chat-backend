54639549bf68e1ffca281f5e836824b0e6b066a3
PawelRadawiec <pawel.radaw@gmail.com>
2020-03-27 12:03:13 +0100

    initial commit

---
+++ src/main/java/info/mychatbackend/MyChatBackendApplication.java
@@ -0,0 +1,13 @@
+package info.mychatbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyChatBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyChatBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/info/mychatbackend/MyChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package info.mychatbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyChatBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
