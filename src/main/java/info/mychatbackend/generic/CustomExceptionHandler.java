d0a2037f84f0c96d09c1073602c6396ef470e6e8
PawelRadawiec <pawel.radaw@gmail.com>
2020-05-10 12:22:37 +0200

    clean up, deleted old class, directory

--- src/main/java/info/mychatbackend/generic/CustomExceptionHandler.java
+++ src/main/java/info/mychatbackend/generic/CustomExceptionHandler.java
@@ -7,37 +7,25 @@ import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
-//    @ExceptionHandler(Exception.class)
-//    public final ResponseEntity<Object> handleAllExceptions(Exception ex, WebRequest request) {
-//        List<String> details = new ArrayList<>();
-//        details.add(ex.getLocalizedMessage());
-//        return new ResponseEntity<>(details, HttpStatus.INTERNAL_SERVER_ERROR);
-//    }
-
     @Override
-    protected ResponseEntity<Object> handleMethodArgumentNotValid(
-            MethodArgumentNotValidException ex, HttpHeaders headers,
-            HttpStatus status, WebRequest request
-    ) {
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
         Map<String, String> invalidFields = new HashMap<>();
-        for (ObjectError error : ex.getBindingResult().getAllErrors()) {
-            String field = ((FieldError) error).getField();
-            invalidFields.put(field, error.getDefaultMessage());
-        }
+        ex.getBindingResult().getAllErrors().forEach(e -> createError(e, invalidFields));
         return new ResponseEntity<>(invalidFields, HttpStatus.BAD_REQUEST);
     }
 
+    private void createError(ObjectError error, Map<String, String> invalidFields) {
+        String field = ((FieldError) error).getField();
+        invalidFields.put(field, error.getDefaultMessage());
+    }
 
 }
